c4504e828df8ce373a536c7582c3653971acba42
lxs <lxs137@hotmail.com>
2017-11-22 00:29:32 +0800

    framework can launch scrapy docker

--- start.sh
+++ start.sh
@@ -9,5 +9,7 @@ docker run -e "SCRAPY_SETTINGS_MODULE=base.configs.wangyi_menhu.settings" -e "MO
 
 docker run -e "SCRAPY_SETTINGS_MODULE=base.configs.wangyi_menhu.settings" -e "MONGO_HOST=114.212.189.147" -e "MONGO_PORT=10094" -e "MONGO_DBNAME=Crawler" -e "PYTHONPATH=/root/Crawler" -d --log-driver=fluentd --log-opt fluentd-address=114.212.189.147:10050 --log-opt tag=docker.{{.ID}} --log-opt fluentd-async-connect scrapy-crawler:latest sh -c 'scrapy crawl spider'
 
+java -classpath target/cf-tutorial-mesos-docker-1.0-SNAPSHOT-jar-with-dependencies.jar -Djava.library.path=/Users/lxs/Library/mesos/build/src/.libs com.codefutures.tutorial.mesos.docker.ExampleFramework 127.0.0.1:5050 scrapy-crawler:latest 1
+
 
 docker run -d -p 24224:24224 -p 24224:24224/udp -v /Users/lxs/workspace/data:/fluentd/log fluent/fluentd:latest

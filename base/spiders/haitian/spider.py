5e8457a3bbd38962a2362645322e4d27b1b4cb49
tinax <Tinax@tinax.local>
2017-11-27 15:56:35 +0800

    Haitian Kaoyan & Wendu Kaoyan

---
+++ base/spiders/haitian/spider.py
@@ -0,0 +1,70 @@
+import scrapy
+
+from base.items.haitian.items import HaiItem
+from scrapy.http import Request
+import datetime
+from base.items.haitian.BloomFilter import BloomFilter
+
+class htkaoyan(scrapy.Spider):
+	name="spider"
+	allowed_domains=["www.htkaoyan.com"]
+
+	start_urls=["http://www.htkaoyan.com"]
+
+	def parse(self,response):
+		self.bf=BloomFilter(0.0001,1000000)
+		while 1:
+			urls = response.xpath("//*/a[starts-with(@href,'http')]/@href").extract()
+			for url in urls:
+				if(self.bf.is_element_exist(url)==False):
+					yield Request(url,callback=self.parse_inPage)
+				else:
+					continue
+
+	def parse_inPage(self,response):
+
+		item = HaiItem()
+		item['title'] = ''
+		item['source'] = "HaiTianKaoYan"
+		item['source_url'] = "http://www.htkaoyan.com"
+		item['url'] = response.url
+		item['html'] = ''
+		item['time'] = ''
+		item['content'] = ''
+		item['attention'] = 0
+		item['sentiment'] = 0
+
+		self.bf.insert_element(response.url)
+
+		contentlist = response.xpath('//html').extract()
+
+		for con in contentlist:
+			utfcontent = con.encode('utf-8')
+			item['html'] += utfcontent
+		titlelist = response.xpath('//title/text()').extract()
+
+		for line in titlelist:
+			title = line.encode('utf-8')
+			item['title'] += title
+
+		timelist = response.xpath("//*/div[@class='c_subtitle']//text()").extract_first()
+		if timelist is None:
+			item['time'] = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+		else:
+			i=0
+			while(i<=18):
+				timelist = response.xpath("//*/div[@class='c_subtitle']//text()").extract_first()[i + 18]
+				if timelist=='-' or timelist==' ' or timelist==':':
+					item['time'] += '_'
+				else:
+					item['time'] += timelist
+				i+=1
+
+		contentlist = response.xpath("//*/div[@class='c_show_content']//text()").extract()
+		for con in contentlist:
+			content = con.encode('utf-8')
+			item['content'] += content
+
+		yield item
+
+
---
+++ base/spiders/wendu/spider.py
@@ -0,0 +1,73 @@
+import scrapy
+
+from base.items.wendu.items import WenduItem
+from scrapy.http import Request
+import datetime
+from base.items.wendu.BloomFilter import BloomFilter
+
+class wendu(scrapy.Spider):
+	name="spider"
+	allowed_domains=["kaoyan.wendu.com"]
+
+	start_urls=["http://kaoyan.wendu.com"]
+
+	def parse(self,response):
+		self.bf=BloomFilter(0.0001,1000000)
+		while 1:
+			urls = response.xpath("//*/a[starts-with(@href,'http')]/@href").extract()
+			for url in urls:
+				urlc=''
+				for urllist in url:
+					urll = urllist.encode('utf-8')
+					urlc += urll
+				if(self.bf.is_element_exist(urlc)==False):
+					yield Request(url,callback=self.parse_inPage)
+				else:
+					continue
+
+	def parse_inPage(self,response):
+
+		item = WenduItem()
+		item['title'] = ''
+		item['source'] = "WenDuKaoYan"
+		item['source_url'] = "http://kaoyan.wendu.com"
+		item['url'] = response.url
+		item['html'] = ''
+		item['time'] = ''
+		item['content'] = ''
+		item['attention'] = 0
+		item['sentiment'] = 0
+
+		self.bf.insert_element(response.url)
+
+		contentlist = response.xpath('//html').extract()
+		for con in contentlist:
+			utfcontent = con.encode('utf-8')
+			item['html'] += utfcontent
+
+		titlelist = response.xpath('//title/text()').extract()
+		for line in titlelist:
+			title = line.encode('utf-8')
+			item['title'] += title
+
+		timelist = response.xpath("//span[@class='article-time']/text()").extract_first()
+		if timelist is None:
+			item['time'] = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+		else:
+			i = 0
+			while (i <= 15):
+				timelist = response.xpath("//span[@class='article-time']/text()").extract_first()[i]
+				if timelist == '-' or timelist == ' ' or timelist == ':':
+					item['time'] += '_'
+				else:
+					item['time'] += timelist
+				i += 1
+			item['time'] += '_00'
+
+		contentlist = response.xpath("//*/div[@id='content']//text()").extract()
+		for con in contentlist:
+			content = con.encode('utf-8')
+			item['content'] += content
+
+		yield item
+

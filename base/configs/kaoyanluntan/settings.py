323cc2e2e77bb3ce01a829b21a749f0350fedd83
qianlanwyd <646842131@qq.com>
2017-11-28 16:32:38 +0800

    kaoyanluntan
    remove ; in settings

--- base/configs/kaoyanluntan/settings.py
+++ base/configs/kaoyanluntan/settings.py
@@ -8,8 +8,8 @@
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-import os;
-from datetime import datetime;
+import os
+from datetime import datetime
 
 BOT_NAME = 'kaoyanluntan'
 

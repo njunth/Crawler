a3acb18bb57f3b59fd45eeff9f8d6396fd046cf8
njunth <njunth@163.com>
2017-12-23 10:36:16 +0800

    weibo time

--- base/pipelines/weibo/pipelines.py
+++ base/pipelines/weibo/pipelines.py
@@ -5,11 +5,9 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymongo
-# import time
-import datetime
+import time
 from base.configs.weibo.settings import MONGO_HOST,MONGO_PORT,MONGODB_COLLECTION,MONGODB_DBNAME
 import sys
-import pytz
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -18,7 +16,6 @@ class WeiboPipeline(object):
         client = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
         db = client[MONGODB_DBNAME]
         self.collection = db[MONGODB_COLLECTION]
-        self.tz = pytz.timezone('Asia/Shanghai')
         #self.collection.remove({})
 
     def process_item(self, item, spider):
@@ -39,10 +36,7 @@ class WeiboPipeline(object):
         #keyword = (unicode(keyword) + unicode(item['keyword']) + '_').encode('UTF-8')
         else:
             keyword = item['keyword']
-            d = datetime.datetime.now(self.tz)
-            createstr = d.strftime('%Y_%m_%d_%H_%M_%S')
-            print createstr
-            # createstr = time.strftime('%Y_%m_%d_%H_%M_%S',time.localtime(time.time()))
+            createstr = time.strftime('%Y_%m_%d_%H_%M_%S',time.localtime(time.time()))
         #print keyword
         self.collection.save({
             '_id': item['_id'],

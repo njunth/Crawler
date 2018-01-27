8c2b034edbd50261eef12c81d181fbf5606bb483
njunth <njunth@163.com>
2018-01-27 21:55:30 +0800

    baidusousuo

---
+++ base/configs/baidusousuo/settings.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapy_baidu project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+import os;
+
+BOT_NAME = 'baidusousuo'
+
+SPIDER_MODULES = ['base.spiders.baidusousuo']
+NEWSPIDER_MODULE = 'base.spiders.baidusousuo'
+
+MONGODB_HOST = os.getenv("MONGO_HOST", "localhost")
+MONGODB_PORT = (int)(os.getenv("MONGO_PORT", 27017))
+MONGODB_DBNAME = os.getenv("MONGO_DBNAME", "Crawler")
+MONGODB_COLLECTION = os.getenv("MONGODB_COLLECTION", "baidusousuo")
+
+MYSQL_HOST = os.getenv("MYSQL_HOST", "114.212.189.147")
+MYSQL_PORT = (int)(os.getenv("MYSQL_PORT", 10103))
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "woodpecker")
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "crawl_nju903")
+MYSQL_TABLE = 'keyword_t'
+
+LOG_ENABLED = False
+
+EXTENSIONS = {
+    'scrapy.extensions.logstats.LogStats': None,
+    'base.configs.logstats.LogStats': 150,
+}
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapy_baidu (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'scrapy_baidu.middlewares.ScrapyBaiduSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'scrapy_baidu.middlewares.MyCustomDownloaderMiddleware': 543,
+#}
+
+# Enable or disable extensions
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+    'base.pipelines.baidusousuo.pipelines.ScrapyBaiduPipeline': 300,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
---
+++ base/spiders/baidusousuo/baiduspider.py
@@ -0,0 +1,79 @@
+# -*-coding:utf-8-*-
+import scrapy
+from scrapy.spiders import Spider
+import pyreBloom
+from base.configs.settings import REDIS_HOST, REDIS_PORT
+from base.items.baidusousuo.items import ScrapyBaiduItem
+from base.configs.baidusousuo.settings import MYSQL_HOST, MYSQL_PORT, MYSQL_DATABASE, MYSQL_TABLE, MYSQL_USER, MYSQL_PASSWORD
+import time
+import datetime
+import MySQLdb
+import sys
+import os
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class BaiduSpider(Spider):
+    name = 'spider'
+    def __init__(self):
+        # self.bf = BloomFilter(0.0001, 100000)
+        self.bf = pyreBloom.pyreBloom( 'baidusousuo', 100000, 0.0001, host=REDIS_HOST, port=REDIS_PORT )
+
+    def start_requests(self):
+        os.environ["all_proxy"] = "http://dailaoshi:D9xvyfrgPwqBx39u@bh21.84684.net:21026"
+        headers = {
+            'Host': 'www.baidu.com',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Accept-Encoding': 'gzip, deflate, br',
+            #'Cookie': 'BDUSS=mU5b0JMcDRsTG90WWpTaU1aS2Iyd2N4QjhDNDlDSXNGVHpqQ3I5RUcwMEZUTDFaTVFBQUFBJCQAAAAAAAAAAAEAAADIYkwTendt1cUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAW~lVkFv5VZa; BAIDUID=9E7D54EE7C5D10298D4BA6C6272F4311:FG=1; BIDUPSID=6A549A7B4CFCB978124826509C41AD0C; PSTM=1509457426; MCITY=-233%3A; ispeed_lsm=1; BD_UPN=1352; ispeed=1; H_PS_PSSID=1435_21090_17001_20927; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; sugstore=1; BD_CK_SAM=1; PSINO=1; BDSVRTM=0',
+            #'Connection': 'keep-alive',
+            #'Upgrade-Insecure-Requests': '1'
+        }
+        # keywords = ['提供', '发现',  '研考', '硕士考试', '硕士生考试', '成考', '成人高考', '自考', '自学考试']
+        while 1:
+            db = MySQLdb.connect( host=MYSQL_HOST, port=MYSQL_PORT, user=MYSQL_USER, passwd=MYSQL_PASSWORD,
+                                  db=MYSQL_DATABASE, charset='utf8' )
+            cursor = db.cursor()
+            sql = "SELECT * FROM keyword_t"
+            cursor.execute( sql )
+            keywords = cursor.fetchall()
+            url_p1 = 'https://www.baidu.com/s?wd='
+            url_p2 = '&pn='
+            for i in range(20, 0, -1):
+                # print i
+                for keyword in keywords[::-1]:
+                    # print keyword[1]
+                    url = url_p1 + keyword[1] + url_p2 + str(i*10)
+                    time.sleep(1)
+                    yield scrapy.Request(url=url,headers=headers,dont_filter=True,callback=self.parse,meta={'keyword':keyword[1]})
+
+    def parse(self, response):
+        # print response.text
+        keyword = response.meta['keyword']
+        results = response.xpath('//div[@class="result c-container "]')
+        #print results
+        time = datetime.datetime.now()
+        for res in results:
+            #print res.extract()
+            url = res.xpath('.//h3[contains(@class,"t")]/a/@href').extract_first()
+            # print keyword,url
+            bfdata = str(keyword) + str(url)
+            if (self.bf.contains(str(bfdata)) == False):
+                self.bf.extend(str(bfdata))
+                item = ScrapyBaiduItem()
+                item['url'] = url
+                item['title'] = res.xpath('.//h3[contains(@class,"t")]/a').xpath('string(.)').extract_first()
+                #print title
+                abstract = res.xpath('.//div[contains(@class,"c-abstract")]').xpath('string(.)').extract_first()
+                if abstract == None:
+                    #print res.extract()
+                    abstract = res.xpath('.//div[@class="c-span18 c-span-last"]/font/p').xpath('string(.)').extract()
+                    abstract = ' '.join(abstract)
+                    #print abstract
+                #print abstract
+                item['abstract'] = abstract
+                item['keyword'] = unicode(keyword)
+                item['time'] = time.strftime('%Y_%m_%d_%H_%M_%S')
+                yield item
